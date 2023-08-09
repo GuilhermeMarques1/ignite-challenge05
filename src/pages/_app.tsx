c99a2c5d99d23a47a8dd986b011a65463e2f2cd5
GuilhermeMarques1 <g-marques@outlook.com>
2023-08-09 01:18:14 -0300

    fix: Remove title from document and move to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import { AppProps } from 'next/app';
 import Header from '../components/Header';
 
 import '../styles/globals.scss';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
+      <Head>
+        <title>spacetraveling</title>
+      </Head>
       <Header/>
       <Component {...pageProps} />
     </>
